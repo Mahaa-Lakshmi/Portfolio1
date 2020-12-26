5ff3374d0ce3d4a2665cfedf0dd7aec5ea7b44ff
Mahaalakshmi <59535770+Mahaa-Lakshmi@users.noreply.github.com>
2020-12-26 23:44:06 +0530

    Add files via upload

--- fun.js
+++ fun.js
@@ -1,9 +1,12 @@
+//timeline
+
 $('.member-title').click(function(e) {
   console.log("Clicked");
   $(this).next().slideToggle();
   $(this).next().next().next().slideToggle();
 })
 
+//Topbar
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -11,4 +14,56 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+//certificates
+//var slideIndex = 1;
+//showSlides(slideIndex);
+
+var slideIndex = 0;
+showSlides();
+
+/*
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}*/
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides() {
+	
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3500); // Change image every 2 seconds
+/*
+
+var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3500);
+  */
 }
